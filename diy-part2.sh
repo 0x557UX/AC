14ced034bc18973f7503f873e7557d74aca3cfae
X <ghawp@protonmail.com>
2025-10-01 04:35:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,3 +3,4 @@
 sed -i 's/192.168.1.1/192.168.30.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/ASUS/g' package/base-files/files/bin/config_generate
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+sed -i 's/0x4000000/0x7000000/g' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1.dts
